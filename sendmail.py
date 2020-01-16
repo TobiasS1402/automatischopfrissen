77b8c5b8ac42265e9def434a7272d787c806a072
TobiASS <46230851+TobiasS1402@users.noreply.github.com>
2020-01-16 21:13:34 +0100

    Email security en customization via office365 SMTP

--- sendmail.py
+++ sendmail.py
@@ -1,13 +1,16 @@
 import smtplib
+from getpass import getpass
 
 
 def mailfunctie(SUBJECT, TEXT):
 
     message = 'Subject: {}\n\n{}'.format(SUBJECT, TEXT)
-    
-    office365Pass = input("Geef hier je Office365 password op")
+
+    office365Usr = input("Geef hier je gebruikersnaam op ")
+    office365Pass = getpass("Geef hier je Office365 password op ")
+
     server = smtplib.SMTP('smtp.office365.com', 587)
     server.starttls()
-    server.login("tobias.seijsener@student.hu.nl", office365Pass)
-    server.sendmail('tobias.seijsener@student.hu.nl','toseijs@gmail.com',message)
+    server.login(office365Usr, office365Pass)
+    server.sendmail(office365Usr,'toseijs@gmail.com',message)
     server.quit()
